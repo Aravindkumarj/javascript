19ca29d7493243e4df0f7afab7d79ecb22f32a11
Aravind Kumar Jagannathan <aravind.kumar.jagannathan@sap.com>
2020-05-23 17:48:18 -0500

    2:23 - tip calculator

---
+++ tipCalculator.js
@@ -0,0 +1,23 @@
+var bills = [124, 48, 268]
+function tipCalculator(a,b,c){
+	tips = [];
+	totalBill = [];
+	for (i = 0; i < arguments.length ; i++)
+	{
+		if (arguments[i] < 50){
+			tips[i] = arguments[i] * 0.2;
+			totalBill[i] = arguments[i]+ tips[i];
+		}
+		else if ((arguments[i]>= 50) && (arguments[i] <200)){
+			tips[i] = arguments[i] * 0.5;
+			totalBill[i] = arguments[i]+ tips[i];
+		}
+		else if (arguments[i]>= 200){
+			tips[i] = arguments[i] * 0.1;
+			totalBill[i] = arguments[i]+ tips[i];
+		}
+	}
+	console.log (tips, totalBill);
+	return (tips, totalBill);
+}
+tipCalculator(...bills);
